f873729ebac4217dd39e617492d559b83e5badd3
Mimiview <Mimiview@github.com>
2021-03-30 23:28:37 +0200

    finished the pyflow, but need to fix some bugs

--- autoclass.py
+++ autoclass.py
@@ -7,7 +7,25 @@ web_http = ["https://moodle.unive.it/course/view.php?id=5616", "https://moodle.u
 
 pixel_lessons = [[-1460, 455], [-1396, 658], [-1444, 872], [-1464, 469], [-1444, 538]]
 
-pixel_zoom_lesson = []
+# TODO vedere tempo di responsing della pagina, mini problemi
+
+# sorta di main momentaneo
+def open_windows():
+    i = input("Scegliere tra \n1.Basi di dati \n2.1IUM \n3.SO \n4.ASD Pelillo \n5.PO2\n")
+    i = int(i)-1
+    j = input("Hai già effettuato il login a moodle? Y/n ")
+    open_chrome()
+    web_links(i)
+    if (j != "Y") or (j != "y"):  # TODO capire in che modo risolvere il fatto se si è collegati o meno
+        print('flaggone matto')
+        login_page()
+    else:
+        time.sleep(1)
+    # search di lezioni da caFebbraio mbiare posizione per ognuna, a indice dei siti web corrisponderà la propria
+    # posizione
+    crtl_shortcut('f')
+    findLinkLesson(i, "lezioni")
+    zoom_in(i)
 
 
 # debugger per la posizione
@@ -19,34 +37,20 @@ def positioning():
 
 # questo è duro, ognuno ha il link per entrare sul portale zoom in posti diversi di moodle
 def findLinkLesson(i, j):
-    pyautogui.keyDown('ctrl')
-    pyautogui.press('f')
-    pyautogui.keyUp('ctrl')
     pyautogui.typewrite(j)
-    pyautogui.click(pixel_lessons[i][0], pixel_lessons[i][1])
+    pyautogui.click(pixel_lessons[i][0], pixel_lessons[i][1])  # click Calendario lezioni
+    time.sleep(1)
 
 
 # apre chrome, nel mio caso si trova nella barra delle applicazioni
 def open_chrome():
-    pyautogui.click(-1562, 1076)
+    pyautogui.click(-1562, 1076)  # click la barra delle applicazioni
     time.sleep(1)
-    pyautogui.click(-1596, 52)
-
-
-# sorta di main momentaneo
-def open_windows():
-    i = input("Scegliere tra 1. Basi di dati 2. IUM 3.SO 4.ASD Pelillo 5.PO2")
-    i = int(i)
-
-    open_chrome()
-    web_links(i)
-    login_page()
-    # search di lezioni da cambiare posizione per ognuna, a indice dei siti web corrisponderà la propria posizione
-    findLinkLesson(1, "lesson")
 
 
 # scrive sulla barra di nav. i link delle lezioni
 def web_links(i):
+    pyautogui.click(-1596, 52)  # barra di navigazione
     pyautogui.typewrite(web_http[i])
     pyautogui.press('enter')
 
@@ -54,21 +58,39 @@ def web_links(i):
 # utilizzare per fare il login in moodle (le credenziali salvale)
 def login_page():
     time.sleep(4)
-    pyautogui.click(-1536, 434)
+    pyautogui.click(-1536, 434)  # prima del login
     time.sleep(2)
-    pyautogui.click(-1043, 585)
+    pyautogui.click(-1043, 585)  # login
 
 
-# dentro il link di zoom, but TODO AMMA FINI
+# dentro il link di zoom TODO mini problema per quanto riguarda il copying del passcode, vedere bene, e problema sul joi
 def zoom_in(i):
-    pyautogui.click(-132, 377)
-    findLinkLesson(i, "PASS")
+    pyautogui.click(-124, 377)  # invitation
+    time.sleep(2)
+    crtl_shortcut('f')
+    pyautogui.typewrite("")
+    time.sleep(1)
+    pyautogui.doubleClick(-1129, 487)  # posizione del passcode
+    time.sleep(2)
+    crtl_shortcut('c')
+    time.sleep(2)
+    pyautogui.click(-216, 376)  # posizione del join
+    time.sleep(0.5)
+    pyautogui.click(-216, 376)  # posizione del join
+    time.sleep(8)
+    pyautogui.click(828, 481)  # posizione della password
+    crtl_shortcut('v')
+    pyautogui.click(945, 657)  # poszione bottone per entrare
+
+
+# shortucut find
+def crtl_shortcut(letter):
+    pyautogui.keyDown('ctrl')
+    pyautogui.press(letter)
+    pyautogui.keyUp('ctrl')
+
+
+open_windows()
 
 
 # positioning()
-
-
-# open_windows()
-
-
-positioning()
