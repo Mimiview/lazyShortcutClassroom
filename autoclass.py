92c7749e02174b27d9aa09f5221e30763f526aed
Mimiview <Mimiview@github.com>
2021-03-30 21:01:34 +0200

    fatto un po di ordine separando in funzioni, addato l'input

--- autoclass.py
+++ autoclass.py
@@ -7,43 +7,68 @@ web_http = ["https://moodle.unive.it/course/view.php?id=5616", "https://moodle.u
 
 pixel_lessons = [[-1460, 455], [-1396, 658], [-1444, 872], [-1464, 469], [-1444, 538]]
 
+pixel_zoom_lesson = []
 
+
+# debugger per la posizione
 def positioning():
     for i in range(10):
         time.sleep(1)
         print(pyautogui.position())
 
 
-def press_search():
+# questo è duro, ognuno ha il link per entrare sul portale zoom in posti diversi di moodle
+def findLinkLesson(i, j):
     pyautogui.keyDown('ctrl')
     pyautogui.press('f')
     pyautogui.keyUp('ctrl')
+    pyautogui.typewrite(j)
+    pyautogui.click(pixel_lessons[i][0], pixel_lessons[i][1])
 
 
-def open_windows():
+# apre chrome, nel mio caso si trova nella barra delle applicazioni
+def open_chrome():
     pyautogui.click(-1562, 1076)
-    time.sleep(0.5)
+    time.sleep(1)
     pyautogui.click(-1596, 52)
-    pyautogui.typewrite(web_http[2])
+
+
+# sorta di main momentaneo
+def open_windows():
+    i = input("Scegliere tra 1. Basi di dati 2. IUM 3.SO 4.ASD Pelillo 5.PO2")
+    i = int(i)
+
+    open_chrome()
+    web_links(i)
+    login_page()
+    # search di lezioni da cambiare posizione per ognuna, a indice dei siti web corrisponderà la propria posizione
+    findLinkLesson(1, "lesson")
+
+
+# scrive sulla barra di nav. i link delle lezioni
+def web_links(i):
+    pyautogui.typewrite(web_http[i])
     pyautogui.press('enter')
-    # login page
-    time.sleep(3)
+
+
+# utilizzare per fare il login in moodle (le credenziali salvale)
+def login_page():
+    time.sleep(4)
     pyautogui.click(-1536, 434)
     time.sleep(2)
     pyautogui.click(-1043, 585)
-    # search di lezioni da cambiare posizione per ognuna, a indice dei siti web corrisponderà la propria posizione
-    press_search()
-    pyautogui.typewrite("lezioni")
-    pyautogui.click(pixel_lessons[2][0], pixel_lessons[2][1])  # qui bisogna cambiare
 
-    # TODO da completare la parte di zoom e ricordarsi di fare una lista per la posizone del passcode
+
+# dentro il link di zoom, but TODO AMMA FINI
+def zoom_in(i):
+    pyautogui.click(-132, 377)
+    findLinkLesson(i, "PASS")
 
 
 # positioning()
 
 
-open_windows()
+# open_windows()
 
 
 positioning()
-
