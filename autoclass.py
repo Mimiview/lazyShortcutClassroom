1a1b2fe0999582f665e51b9334c7668cc4694f49
Mimiview <Mimiview@github.com>
2021-04-02 12:59:19 +0200

    issue with seleniumm cant fix it, 2 days and ill quit this shit, asked on stackoverflow, searched for 3 days but nothing is gonna work. never give up

--- autoclass.py
+++ autoclass.py
@@ -2,7 +2,8 @@ import time
 import pyautogui
 from selenium import webdriver
 
-driver = webdriver.Chrome("driver/chromedriver")
+
+driver = webdriver.Chrome(r"C:\Users\Alex\Desktop\Python\lazyShortcutClassroom\driver\chromedriver.exe")
 web_http = ["https://moodle.unive.it/mod/lti/launch.php?id=286767",  # BD
             "https://moodle.unive.it/mod/lti/launch.php?id=298281",  # IUM
             "https://moodle.unive.it/mod/lti/launch.php?id=301935",  # SO
@@ -13,8 +14,8 @@ web_http = ["https://moodle.unive.it/mod/lti/launch.php?id=286767",  # BD
 # sorta di main momentaneo
 def open_windows():
     i = input("Scegliere tra \n1.Basi di dati \n2.IUM \n3.SO \n4.ASD Pelillo \n5.PO2\n")
-    i = int(i)-1
-    driver.get(i)
+    i = int(i) - 1
+    driver.get("https://google.com")
     # j = input("Hai già effettuato il login a moodle? Y/n ")
     assert "moodle" in driver.title
     # problema todo vedere per quale motivo si apre un nuovo ambiente di google
@@ -61,7 +62,7 @@ def crtl_shortcut(letter):
     pyautogui.keyUp('ctrl')
 
 
-# open_windows()
+open_windows()
 
 
-positioning()
+# positioning()
