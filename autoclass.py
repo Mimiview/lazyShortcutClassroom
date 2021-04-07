a897a7b7ffc68f14e2bcddf6061edc19f023d960
Mimiview <axelgiu99@gmail.com>
2021-04-07 19:05:55 +0200

    cleaned it

--- autoclass.py
+++ autoclass.py
@@ -2,7 +2,6 @@ import time
 import pyautogui
 from selenium import webdriver
 
-
 driver = webdriver.Chrome("driver/chromedriver.exe")
 web_http = ["https://moodle.unive.it/mod/lti/launch.php?id=286767",  # BD
             "https://moodle.unive.it/mod/lti/launch.php?id=298281",  # IUM
@@ -10,67 +9,40 @@ web_http = ["https://moodle.unive.it/mod/lti/launch.php?id=286767",  # BD
             "https://moodle.unive.it/mod/lti/launch.php?id=229445",  # ASD
             "https://moodle.unive.it/mod/lti/launch.php?id=297564"]  # PO2
 
-username = 'yourusername'+"@"+'stud.unive.it'  # trovare il perchè la chiocciola crea problemi
-password = ""
+username = 'yourusername@stud.unive.it'  # big problem about the @
+password = ""  # one time with a gui we can manage it with a text field
 
 
-# sorta di main momentaneo
-def open_windows():
+def main_fun():
     driver.get(web_http[0])
+    # todo  require a condition to verify if login is required
+    login_fun()
+    driver.find_element_by_link_text("Zoom")
+    # driver.close()
+
+
+def login_fun():
     # click of the login unive main page, modify with a condition about the login. Used xpath because couldnt find his class name
     driver.find_element_by_xpath('//*[@id="region-main"]/div[1]/div/div/div/div/div/div/div[1]/a').click()
-
     driver.find_element_by_id("username").send_keys(username)
     driver.find_element_by_id("password").send_keys(password)
     driver.find_element_by_name("_eventId_proceed").click()
-    # problema per quanto riguarda il click dell'elemnto per entrare nel moodle
-    # driver.close()
-    # j = input("Hai già effettuato il login a moodle? Y/n ")
-    # todo completare tutto ciò
 
 
-# debugger per la posizione
+# position debugger
 def positioning():
     for i in range(10):
         time.sleep(1)
         print(pyautogui.position())
 
 
-# utilizzare per fare il login in moodle (le credenziali salvale)
-def login_page():
-    time.sleep(4)
-    pyautogui.click(-1536, 434)  # prima del login
-    time.sleep(2)
-    pyautogui.click(-1043, 585)  # login
-
-
-def zoom_in(i):
-    pyautogui.click(-124, 377)  # invitation
-    time.sleep(2)
-    crtl_shortcut('f')
-    pyautogui.typewrite("")
-    time.sleep(1)
-    pyautogui.doubleClick(-1129, 487)  # posizione del passcode
-    time.sleep(2)
-    crtl_shortcut('c')
-    time.sleep(2)
-    pyautogui.click(-216, 376)  # posizione del join
-    time.sleep(0.5)
-    pyautogui.click(-216, 376)  # posizione del join
-    time.sleep(8)
-    pyautogui.click(828, 481)  # posizione della password
-    crtl_shortcut('v')
-    pyautogui.click(945, 657)  # poszione bottone per entrare
-
-
-# shortucut find
+# find shortcut
 def crtl_shortcut(letter):
     pyautogui.keyDown('ctrl')
     pyautogui.press(letter)
     pyautogui.keyUp('ctrl')
 
 
-open_windows()
-
+main_fun()
 
 # positioning()
