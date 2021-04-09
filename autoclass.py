d48c19991fdedfc23166978004f8202bf4846f84
Mimiview <Mimiview@github.com>
2021-04-09 19:16:37 +0200

    button invitation problem not solved, tried requesting but cant access it

--- autoclass.py
+++ autoclass.py
@@ -1,5 +1,6 @@
 import time
 import pyautogui
+import requests as req
 from selenium import webdriver
 
 driver = webdriver.Chrome("driver/chromedriver.exe")
@@ -9,7 +10,7 @@ web_http = ["https://moodle.unive.it/mod/lti/launch.php?id=286767",  # BD
             "https://moodle.unive.it/mod/lti/launch.php?id=229445",  # ASD
             "https://moodle.unive.it/mod/lti/launch.php?id=297564"]  # PO2
 
-username = 'yourusername@stud.unive.it'  # big problem about the @
+username = '882483@stud.unive.it'  # big problem about the @
 password = ''  # one time with a gui we can manage it with a text field
 
 
@@ -23,7 +24,10 @@ def main_fun():
 
 
 def extract_pass():
-    driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div/div[2]/div[2]/textarea').click()
+    # driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div/div[2]/div[2]/textarea').click()
+    resp = req.get("https://applications.zoom.us/api/v1/lti/rich/meeting/88578281421/invitation?lti_scid=ad82b86c546f32146d3c989ec16e8f1a0bfff43c5bbc111fa07dc0d91de9738c")
+    resp = resp.text
+    print(resp)
 
 
 def login_fun():
